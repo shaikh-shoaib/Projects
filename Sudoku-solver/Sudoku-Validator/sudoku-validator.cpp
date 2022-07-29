e378f7533669cb9c0dd2864daee9d476ba16d708
shaikh-shoaib <72928062+shaikh-shoaib@users.noreply.github.com>
2022-07-29 11:43:11 +0530

    Update sudoku-validator.cpp

--- Sudoku-solver/Sudoku-Validator/sudoku-validator.cpp
+++ Sudoku-solver/Sudoku-Validator/sudoku-validator.cpp
@@ -2,9 +2,7 @@
  *========================
  *    Sudoku Validator
  *========================
- *
- * Started On: October 18th, 2018
- * Author: Arjun Aravind
+
  * Objective: To verify whether a completed Sudoku puzzle is valid or not.
 */
 
